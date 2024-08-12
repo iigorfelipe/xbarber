a016c9a103922d8dcdb3377fe91ec9e3df570c8d
Igor <iigorfelipe@gmail.com>
2024-08-12 12:24:29 -0300

    feat: ensure bookings are listed on bookings page

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,37 @@ import { quickSearchOptions } from './_constants/search';
 import BookingItem from './_components/booking-item';
 import Search from './_components/search';
 import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './_lib/auth';
 
 const Home = async () => {
+  const session = await getServerSession(authOptions);
   const barbershops = await db.barbershop.findMany({});
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: 'desc',
     },
   });
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: 'asc',
+        },
+      })
+    : [];
 
   return (
     <div>
@@ -33,7 +56,7 @@ const Home = async () => {
         {/* BUSCA RÁPIDA */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
           {quickSearchOptions.map((option) => (
-            <Button className="shrink-0 gap-2" variant="secondary" key={option.title} asChild>
+            <Button className="gap-2" variant="secondary" key={option.title} asChild>
               <Link href={`/barbershops?service=${option.title}`}>
                 <Image src={option.imageUrl} width={16} height={16} alt={option.title} />
                 {option.title}
@@ -52,8 +75,14 @@ const Home = async () => {
           />
         </div>
 
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">Agendamentos</h2>
+
         {/* AGENDAMENTO */}
-        <BookingItem />
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">Recomendados</h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
