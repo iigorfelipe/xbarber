4cdf2538bdd6e4f4e375fcfdebb75724f92cbf70
Igor <iigorfelipe@gmail.com>
2024-08-12 12:35:19 -0300

    feat: ensure booking can be canceled

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -1,9 +1,9 @@
 'use server';
 
-import { getServerSession } from 'next-auth';
 import { revalidatePath } from 'next/cache';
-import { authOptions } from '../_lib/auth';
 import { db } from '../_lib/prisma';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../_lib/auth';
 
 interface CreateBookingParams {
   serviceId: string;
@@ -12,11 +12,12 @@ interface CreateBookingParams {
 
 export const createBooking = async (params: CreateBookingParams) => {
   const user = await getServerSession(authOptions);
-  if (!user) { 
+  if (!user) {
     throw new Error('Usuário não autenticado');
   }
   await db.booking.create({
     data: { ...params, userId: (user.user as any).id },
   });
   revalidatePath('/barbershops/[id]');
+  revalidatePath('/bookings');
 };
